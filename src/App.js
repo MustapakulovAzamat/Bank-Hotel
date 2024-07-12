88986bf0880283c5a191784daf2a44d1b620ca52
Mustapakulov Azamat <azamustapakylov6@gmail.com>
2024-07-12 20:41:11 +0600

    display all pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from "./components/Footer/Footer";
+import { AboutPage } from "./pages/AboutPage/AboutPage";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { OurPage } from "./pages/OurPage/OurPage";
+import { RoomsPage } from "./pages/RoomsPage/RoomsPage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainPage/>
+      <AboutPage/>
+      <RoomsPage/>
+      <OurPage/>
+      <Footer/>
     </div>
   );
 }
